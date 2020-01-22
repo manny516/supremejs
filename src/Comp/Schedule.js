7749ce4381d8972bca2aae8d47ba0b72fde0f8e3
Immanuel  Walthrust <mwalthrust@gmail.com>
2020-01-22 18:33:38 -0500

    updated schedule code

--- src/Comp/Schedule.js
+++ src/Comp/Schedule.js
@@ -1,35 +1,67 @@
 class Schedule{
 
     constructor(barberName,image, services,socialMedia){
-
-        this.barberName = barberName; //@Param Expects a return String 
-        this.image = image; //@Param Expects a return String 
-        this.services = services;   //@Param Expects a return Array
-        this.socialMedia = socialMedia; //@Param Expects a return Array
+        /** 
+         * 
+         * @param barberIdentity  return String   
+         * @param image  return URL String 
+         * @param services  return Array for services input
+         * @param socialMedia return Object
+         **/
+        
+        this.barberName = barberName; 
+        this.image = image;
+        this.services = services;
+        this.socialMedia = socialMedia;
 
     }
 
+
+    servicesCheckbox(){
+        this.services.forEach( (item)=>{
+            let serviceCheck = `
+            <input type="checkbox" name="${item}" value="${item}" />${item}
+            `;
+            return serviceCheck;
+        });
+    }
+
+    socialMediaLinks(){
+        this.socialMedia.map((socialItem) =>{
+            let socialLink = `
+                <a href="${socialItem.facebook}" class="fa-facebook">  </a>
+                <a href="${socialItem.instagram}" class="fa-instagram">  </a>
+                <a href="${socialItem.twitter}" class="fa-twitter">  </a>
+            `;
+            return socialLink;
+        });
+    }
 
     barberIdentity(){
 
         const barberTitle = `
-        <div class="barber-sch">
+        <section class="barber-sch">
             <h1>${this.barberName}</h1>
-            
-        </div>
+
+            <figure>
+                <img src="${this.image}" alt="Barber Image" />
+            </figure>
+
+            <section class="barber-services"> 
         
+            </section>
+            
+            <section class="barber-social">
+                <nav>
+                  
+                </nav>
+            </section>
+        </section>
         `;
 
-
+        return barberTitle;
     }
-
-
-
-
-
-
+ 
 }
-
-
 
 export {Schedule};
