025f5f177d00b4c5a2a6d545043ff45d57e1edaf
Immanuel  Walthrust <mwalthrust@gmail.com>
2020-01-22 18:36:03 -0500

    added Class instance

--- src/Comp/View.js
+++ src/Comp/View.js
@@ -1,4 +1,27 @@
+
+import {Schedule} from './Comp/Schedule';
 let data = "<p> Hello this is test </p>";
+
+
+const barberSchedule = new Schedule("newItem.name","newItem.userImage");;
+// console.log(barberSchedule.barberIdentity());
+let returnData = fetch('./barberData.json')
+.then((response) => response.json())
+.then((data) =>{
+    let userName ;
+    let userImage;
+    const barberSchedule 
+    data.map((items) =>{
+        const items$= new Schedule(userName,userImage);
+        userName = items.name;
+        userImage = items.userImage;
+        console.log(barberSchedule.barberIdentity());
+    });
+    
+    
+
+});
+
 
 
 export {data};
