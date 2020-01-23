ebcf00933c240739d966b3b234d9e2c19fe819c5
Immanuel  Walthrust <mwalthrust@gmail.com>
2020-01-23 17:13:32 -0500

    Output Barber schedule data to front page

--- src/Comp/View.js
+++ src/Comp/View.js
@@ -1,4 +1,27 @@
-let data = "<p> Hello this is test </p>";
+import './css/main.scss';
+import datepicker from 'js-datepicker'; 
+import {Schedule} from './Comp/Schedule';
+ 
+ let data = fetch('./barberData.json')
+    .then((response) => response.json())
+    .then((data) =>{
+        var barberSchedule = {};
+        data.forEach((item,index) => { 
+            barberSchedule[index] = new Schedule(item.name,item.userImage,item.services,item.socialMedia);
+            barberSchedule[index] =  barberSchedule[index].barberIdentity();
+            document.getElementById("target").insertAdjacentHTML('afterbegin',barberSchedule[index]);
+            datepicker('.date-picker', {
+                formatter: (input, date, instance) => {
+                    const value = date.toLocaleDateString()
+                    input.value = value // => '1/1/2099'
+                }
+            });
+        });
 
+        let grabAllDataPicks = document.querySelectorAll(".date-picker");
+        console.log(grabAllDataPicks);
+    
+    })
+    .catch(console.error());
 
 export {data};
