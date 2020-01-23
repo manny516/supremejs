8c450d82cf44402b42c3973fa074fbca53a80f2e
Immanuel  Walthrust <mwalthrust@gmail.com>
2020-01-23 17:20:58 -0500

    Refactor : Destructured Constructor params

--- src/Comp/Schedule.js
+++ src/Comp/Schedule.js
@@ -1,16 +1,16 @@
 class Schedule{
 
-    constructor(barberName,image, services,socialMedia){
+    constructor({name,userImage, services,socialMedia}){
         /** 
          * 
-         * @param barberName return String   
-         * @param image  return URL String 
+         * @param name  return String   
+         * @param userImage  return URL String 
          * @param services  return Array for services input
          * @param socialMedia return Object
          **/
         
-        this.barberName = barberName; 
-        this.image = image;
+        this.name = name; 
+        this.userImage = userImage;
         this.services = services;
         this.socialMedia = socialMedia;
 
@@ -18,7 +18,7 @@ class Schedule{
 
 
     servicesCheckbox(){
-        
+        // console.log(this.services.length);
         let inputArray = [];
         for(let i = 0; i < this.services.length; i++){
          inputArray[i] = `<input type="checkbox" name="${this.services[i]}" value="${this.services[i]}" /> ${this.services[i]}`; 
@@ -38,13 +38,12 @@ class Schedule{
 
 
     barberIdentity(){
-        
         const barberTitle = `
         <section class="barber-sch">
-            <h1>${this.barberName}</h1>
+            <h1>${this.name}</h1>
 
             <figure>
-                <img src="${this.image}" alt="Barber Image" />
+                <img src="${this.userImage}" alt="Barber Image" />
             </figure>
 
             <section class="barber-calendar">
